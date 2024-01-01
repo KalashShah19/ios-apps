923242547c38047cd4e115083a6fe0730c905bca
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2024-01-01 10:05:46 +0530

    basic quiz app done

--- quiz app/quiz app/ViewController.swift
+++ quiz app/quiz app/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var TXTquestion: UILabel!
+    
+    @IBOutlet var BTNshow: UIButton!
+    
+    @IBOutlet var TXTanswer: UILabel!
+    
+    @IBOutlet var BTNnext: UIButton!
+    
+    let questions: [String] = [
+            "What is Swift?",
+            "What is the capital of India?",
+            "What is Application made from?"
+    ]
+    
+    let answers: [String] = [
+            "Programming Language for IOS",
+            "Delhi",
+            "Coding"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        TXTquestion.text = questions[currentQuestionIndex]
     }
 
-
+    @IBAction func BTNnext(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        TXTquestion.text = question
+        TXTanswer.text = "Answer"
+    }
+    
+   
+    @IBAction func BTNshow(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+            TXTanswer.text = answer
+    }
 }
 
