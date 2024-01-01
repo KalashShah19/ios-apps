c7df2f964de96da53971ae7a4da7a8c931077760
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2024-01-01 09:12:38 +0530

    quiz app design

---
+++ quiz app/quiz app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz app
+//
+//  Created by bmiit on 01/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
