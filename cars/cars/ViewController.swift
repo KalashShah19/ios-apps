67cd17641e7be782e1e63d55ab6b2f24e1131dae
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2024-01-09 10:34:10 +0530

    improved

--- cars/cars/ViewController.swift
+++ cars/cars/ViewController.swift
@@ -54,10 +54,10 @@ class ViewController: UIViewController {
     
     @IBOutlet var switchDark: UISwitch!
     @IBOutlet var LBLtitle: UILabel!
-    
+    @IBOutlet var TXTinput: UITextField!
     @IBOutlet var image: UIImageView!
-    
     @IBOutlet var LBLdark: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Structure Implementation")
@@ -84,17 +84,35 @@ class ViewController: UIViewController {
             view.backgroundColor = UIColor.black
             LBLtitle.textColor = UIColor.white
             LBLdark.textColor = UIColor.white
+            TXTinput.backgroundColor = UIColor.white
         }
         else{
             view.backgroundColor = UIColor.white
             LBLtitle.textColor = UIColor.black
             LBLdark.textColor = UIColor.black
+            TXTinput.backgroundColor = UIColor.white
         }
     }
     
-    
     @IBAction func audi(_ sender: Any) {
         image.image = UIImage(named:"audi a8")
+    }
+    
+    @IBAction func porche(_ sender: Any) {
+        image.image = UIImage(named:"porche 911")
+    }
+    
+    @IBAction func bugatti(_ sender: Any) {
+        image.image = UIImage(named:"bugatti chiron")
+    }
+    
+    @IBAction func bmw(_ sender: Any) {
+        image.image = UIImage(named:"bmw m2")
+    }
+    
+    @IBAction func typing(_ sender: Any) {
+//        let text = TXTinput.text
+        
     }
 }
 
