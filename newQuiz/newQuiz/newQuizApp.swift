1c7575de7598653a2b790cefc78777a52a0ddb11
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2024-03-19 10:14:52 +0530

    backup

---
+++ newQuiz/newQuiz/newQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  newQuizApp.swift
+//  newQuiz
+//
+//  Created by bmiit on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct newQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
