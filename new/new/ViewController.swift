1c603f1de1e56f628646048be36d1b137366e087
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2024-04-15 10:29:31 +0530

    backup

---
+++ new/new/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by bmiit on 12/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var BTN: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
